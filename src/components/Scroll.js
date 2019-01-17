f79478aec636c53e984a65ecee7d2f45864f6cf8
Sanaulla Shaik <mr.shaiksanastocks@gmail.com>
2019-01-17 22:57:52 +0530

    added homepage and npm run build

---
+++ src/components/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Scroll = (props) => {
+	return (
+		<div style = {{ overflowY: 'Scroll', border: '5px', height: '500px'}}>
+
+			{props.children}
+			
+		</div>
+
+	);
+};
+
+export default Scroll;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +12,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ 
